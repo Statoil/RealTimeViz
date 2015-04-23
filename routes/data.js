55d8e1b4949f3b10c85f5f23404bb64e44595a1c
ajontved <andreas@jontvedt.net>
2015-04-23 08:44:27 +0000

    stuff

--- routes/data.js
+++ routes/data.js
@@ -24,5 +24,48 @@ router.get('/currentvalue', function(req, res) {
     });
 });
 
+/*
+ * GET userlist.
+ */
+router.get('/abc', function(req, res) {
+    
+    fs.readFile('./sampledata/currentvalue.json', 'utf8', function (err,data) {
+        if (err) {
+            return console.log(err);
+        }
+        var jsonRep = JSON.parse(data);
+        
+        for (var i = 0, len = jsonRep.length; i < len; ++i) {
+            var dataItem = jsonRep[i];
+            dataItem.plot0 = new Date();
+            dataItem.plot1 = new Date();
+        }
+ 
+ 
+        res.json(jsonRep);
+    });
+});
+
+
+/*
+ * GET userlist.
+ */
+router.get('/abcd:pa', function(req, res) {
+    
+        var pa = req.params.pa;
+        console.log(pa);
+        var request = require('request');
+        
+        
+        
+        request('http://www.google.com', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            //console.log(body) // Show the HTML for the Google homepage.
+        }
+        
+        var resData = {abcd: "test"}
+        res.json(resData);
+    })
+});
 
 module.exports = router;
