acbbee4f94cacc382deba41b24885bcbc219375d
jrogde <jon.rogde@gmail.com>
2015-04-23 11:28:03 +0000

    created mock samples

--- routes/curves.js
+++ routes/curves.js
@@ -2,14 +2,12 @@ var express = require('express');
 var fs = require("fs");
 var router = express.Router();
 
-/*
- * GET userlist.
- */
+
 router.get('/curvelist', function(req, res) {
-    
-    fs.readFile('./sampledata/curvelist.json', 'utf8', function (err,data) {
+
+    fs.readFile('./sampledata/curvelist.json', 'utf8', function(err, data) {
         if (err) {
-          return console.log(err);
+            return console.log(err);
         }
         var jsonRep = JSON.parse(data);
         res.json(jsonRep);
@@ -19,17 +17,81 @@ router.get('/curvelist', function(req, res) {
 
 //example get with an ID 
 router.get('/curvelist/:wellboreId', function(req, res) {
-    
     var wellboreId = req.params.wellboreId;
-    console.log('curves for wellboreid ' + wellboreId);
-    fs.readFile('./sampledata/curvelist.json', 'utf8', function (err,data) {
-        if (err) {
-          return console.log(err);
+    if (wellboreId == "1") {
+        //create random dataset containing a set of samples
+        var ROP = {
+            name: "ROP",
+            unit: "m/h",
+            min: 0,
+            max: 100
+        };
+        var WOB = {
+            name: "WOB",
+            unit: "tonne",
+            min: 0,
+            max: 20
+        };
+        var BIT_DEPTH = {
+            name: "BIT_DEPTH",
+            unit: "m",
+            min: 0,
+            max: 10000
+        };
+        var HOLE_DEPTH = {
+            name: "HOLE_DEPTH",
+            unit: "m",
+            min: 0,
+            max: 10000
+        };
+
+        var result = [];
+        var numberOfSamples = 10;
+        for (var i = numberOfSamples; i > 0 ; i--) {
+        var curves = [];
+            curves.push({
+                name: ROP.name,
+                unit: ROP.unit,
+                value: getRandomNumber(ROP.min, ROP.max)
+            });
+            curves.push({
+                name: WOB.name,
+                unit: WOB.unit,
+                value: getRandomNumber(WOB.min, WOB.max)
+            });
+            curves.push({
+                name: BIT_DEPTH.name,
+                unit: BIT_DEPTH.unit,
+                value: getRandomNumber(BIT_DEPTH.min, BIT_DEPTH.max)
+            });
+            curves.push({
+                name: HOLE_DEPTH.name,
+                unit: HOLE_DEPTH.unit,
+                value: getRandomNumber(HOLE_DEPTH.min, HOLE_DEPTH.max)
+            });
+
+            result.push({ time: ((new Date().getTime())-(i*5000)), curves: curves});
         }
-        var jsonRep = JSON.parse(data);
-        res.json(jsonRep);
-    });
+
+        res.json(result);
+
+    }
+    else {
+        console.log('curves for wellboreid ' + wellboreId);
+        fs.readFile('./sampledata/curvelist.json', 'utf8', function(err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            var jsonRep = JSON.parse(data);
+            res.json(jsonRep);
+        });
+    }
 });
 
+
+
+function getRandomNumber(min, max){
+   return Math.random()*(max-min)+min;
+}
 
 module.exports = router;
